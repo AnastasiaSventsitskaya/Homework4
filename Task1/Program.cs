ae553731db1c823affae9c74eab70d0ee18a269e
Anastasia Sventsitskaya <anastasiasventsitskaya@MacBook-Air-Anastasia.local>
2022-11-24 22:18:32 +0400

    Натуральная степень числа

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Цикл, который принимает на вход два числа А и В и возводит А в натуральную степень В.
+
+Console.WriteLine("Write number1: ");
+double A = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("Write natural number2: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+
+
+double Power(double num1, int num2) 
+{
+     double result = 1;
+
+     for (int i = 0; i < num2; i++)
+     {
+        result *= num1;
+     }
+     return result;
+}
+
+double res = Power(A, B);
+Console.WriteLine(res);
+
+
